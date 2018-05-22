98293d97e2553f1e2d9984474dba1619fc8eb0c7
Andrew Puglionesi <aop4@pitt.edu>
2018-05-22 18:59:34 -0400

    Force HTTPS protocol

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_sslify import SSLify
 # Initialize the app
 app = Flask(__name__, instance_relative_config=True)
 from app import views #yes, it needs to be down here
 
 # Load app settings from config.py
 app.config.from_object('config')
+
+#force HTTPS protocol
+sslify = SSLify(app)
